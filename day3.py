4d3ce607d52c8302d845174cda97d39aadcba7d8
smshqi <53000493+smshqi@users.noreply.github.com>
2019-08-24 14:37:49 +0300

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,38 @@
+x = 5
+y = "john"
+print (x)
+print (y)
+
+x = 4 # x is of type int
+x = "sally" # x is now of type str
+print(x)
+
+x = "john"
+# is the same as
+x = 'john'
+
+x, y, z = "Orange","Bannana","Cherry"
+print (x)
+print (y)
+print(z)
+
+x = y = z ="Orange"
+print (x)
+print (y)
+print(z)
+
+x = "awesome"
+print("python is" + x)
+
+x = 5
+y = 10
+
+print(x+y)
+
+
+
+
+
+
+
+
