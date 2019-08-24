caa7d7a2c641d70f14861f10a3eebdb1e3978263
smshqi <53000493+smshqi@users.noreply.github.com>
2019-08-24 14:10:28 +0300

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,15 @@
+#day 2
+print("hello world")
+
+#this is a comment
+#written in
+#morre than just one line
+
+print("hello world")
+
+"""
+this is a comment
+woritten in
+more than just one line
+"""
+print("hello ")
