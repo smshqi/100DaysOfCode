e0e81bce67f70135670141ef0d27c3d51afb675b
smshqi <53000493+smshqi@users.noreply.github.com>
2019-08-26 15:08:49 +0300

    Add files via upload

---
+++ DAY 7.py
@@ -0,0 +1,17 @@
+print("Hello")
+print('Hello')
+
+a = "Hello"
+print(a)
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print (a)
+
+a = "Hell,world"
+print(a[1])
+
+b = "Hello,world"
+print(a[2:5])
